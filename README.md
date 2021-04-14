195e063a4a51f021ef2ba2b36aec28a05f3e8eb4
Kevin Dorfer <kevin.dorfer@ethoca.com>
2021-04-14 11:42:38 -0400

    added database and added layout to GUI

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # pybudbook
 Contact book project of train my Python skills.
+
+## Pre-requisites
+* Install PyQt5
+```shell
+pip install PyQt5
+```
--- contacts/views.py
+++ contacts/views.py
@@ -16,6 +16,14 @@ class Window(QMainWindow):
     """Main Window."""
     def __init__(self, parent=None):
         """Initializer."""
+        super().__init__(parent)
+        self.setWindowTitle("Contacts")
+        self.resize(550, 250)
+        self.centralWidget = QWidget()
+        self.setCentralWidget(self.centralWidget)
+        self.layout = QHBoxLayout()
+        self.centralWidget.setLayout(self.layout)
+
         self.setupUI()
 
     def setupUI(self):
